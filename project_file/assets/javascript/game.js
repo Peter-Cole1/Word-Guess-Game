f2918a533669ac71bab0a0fb889cacf02006baad
Peter Cole <Peter_cole1@yahoo.com>
2019-07-01 18:07:42 -0700

    added functionality

--- project_file/assets/javascript/game.js
+++ project_file/assets/javascript/game.js
@@ -1,5 +1,5 @@
 //create array of possible words
-const word = ['Doge', 'Corgi', 'German Shepherd', 'Pitbull', 'Snoop', 'Keyboard Dog', 'Shiba', 'Rich mom dog', 'Pug']
+const word = ['doge', 'corgi', 'shiba', 'pug']
 
 //choose word randomly
 let rng = Math.floor(Math.random() * word.length);
@@ -7,6 +7,11 @@ let chosenWord = word[rng];
 let correctLetter = [];
 let incorrectLetter = [];
 let underScore = [];
+let total = [];
+
+let displayUnderScore = document.getElementsByClassName('underScoreHTML');
+let displayIncorrectGuess = document.getElementsByClassName('incorrectGuess');
+let displayCount = document.getElementsByClassName('countDown');
 
 console.log(chosenWord);
 //create underscores depending on length of chosenWord
@@ -19,14 +24,13 @@ let underScoreLength = () =>
     return underScore;
 }
 
-console.log(underScoreLength());
 
 //user chooses letter from alphabet
 document.addEventListener('keypress', (event) => 
-    {
+{
     let keyword = String.fromCharCode(event.keyCode);
-    console.log(keyword);
-    });
+    //keyword is whatever key is pressed as a guess
+    
 
 
 //if correct, add to correctLetters array
@@ -34,20 +38,41 @@ if (chosenWord.indexOf(keyword) > -1)
     {
         //add correct letter to correctLetter array
         correctLetter.push(keyword);
+        total.push(keyword);
+
         //replace underscore with correct letter
         underScore[chosenWord.indexOf(keyword)] = keyword;
+
+        displayUnderScore[0].innerHTML = underScore.join(' ');
+        displayCount[0].innerHTML = total.length;
+
+    
+        
         //check if chosenWord matches the guess
         if(underScore.join('') == chosenWord)
             {
-                alert("You did the thing");
+                alert("You did the thing!");
             }
-        console.log(correctLetter);
+        
     }
 //if incorrect, add to incorrectLetters array
 else
     {
         incorrectLetter.push(keyword);
+        total.push(keyword);
+        displayIncorrectGuess[0].innerHTML = incorrectLetter.join(', ')
+        displayCount[0].innerHTML = total.length;
+
     }
+    
+
+})
+
+
+displayUnderScore[0].innerHTML = underScoreLength().join(' '); 
+displayIncorrectGuess[0].innerHTML = incorrectLetter.join(',');
+
+
 
 //choose letters until word is completely revealed, or attempts hits "0"
     //if word is revealed, reveal soothing dog pic, and reveal text "you win dawg!"
